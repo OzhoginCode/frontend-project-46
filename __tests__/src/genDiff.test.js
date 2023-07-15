e65996fced365c92c0071d510349f515e76e2892
OzhoginCode <danozh.b@gmail.com>
2023-07-15 20:23:32 +0600

    fix eslint and jest interaction

--- __tests__/src/genDiff.test.js
+++ __tests__/src/genDiff.test.js
@@ -1,21 +1,12 @@
-import genDiff from '../../src/genDiff.js';
 import path from 'path';
+import genDiff from '../../src/genDiff.js';
 import expected from '../../fixtures/expected.js';
-
-const config = {
-  globals: {
-    NODE_OPTIONS: '--experimental-vm-modules',
-  },
-};
-
-export default config;
 
 const relPath1 = './fixtures/file1.json';
 const relPath2 = './fixtures/file2.json';
 
 const absPath1 = path.resolve(relPath1);
 const absPath2 = path.resolve(relPath2);
-
 
 test('gendiff main flow, absolute paths', () => {
   expect(genDiff(absPath1, absPath2)).toBe(expected);
