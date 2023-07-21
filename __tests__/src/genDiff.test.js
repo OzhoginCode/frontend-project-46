ad269fa3dda2aff5cea994facc591ab6fdff9d99
OzhoginCode <danozh.b@gmail.com>
2023-07-21 22:23:35 +0600

    add tests for json formatter

--- __tests__/src/genDiff.test.js
+++ __tests__/src/genDiff.test.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import genDiff from '../../src/genDiff.js';
@@ -12,21 +13,28 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '..', '__fixture
 const jsonPath1 = getFixturePath('file1.json');
 const jsonPath2 = getFixturePath('file2.json');
 
-test('gendiff main flow, json', () => {
+test('gendiff main flow, stylish format, json', () => {
   expect(genDiff(jsonPath1, jsonPath2)).toBe(expected);
 });
 
 const yamlPath1 = getFixturePath('file1.yml');
 const yamlPath2 = getFixturePath('file2.yml');
 
-test('gendiff main flow, yaml', () => {
+test('gendiff main flow, stylish format, yaml', () => {
   expect(genDiff(yamlPath1, yamlPath2)).toBe(expected);
 });
 
-test('gendiff main flow, plain, json', () => {
+test('gendiff main flow, plain format, json', () => {
   expect(genDiff(jsonPath1, jsonPath2, 'plain')).toBe(expected2);
 });
 
-test('gendiff main flow, plain, yml', () => {
+test('gendiff main flow, plain format, yaml', () => {
   expect(genDiff(yamlPath1, yamlPath2, 'plain')).toBe(expected2);
+});
+
+const expected3Path = getFixturePath('expected3.json');
+const expected3 = fs.readFileSync(expected3Path, 'utf8');
+
+test('gendiff main flow, json format, json', () => {
+  expect(genDiff(jsonPath1, jsonPath2, 'json')).toBe(expected3);
 });
