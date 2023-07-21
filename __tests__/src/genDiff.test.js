39b14d14656a68624d9cf5c682c66b109587eff8
OzhoginCode <danozh.b@gmail.com>
2023-07-21 21:48:16 +0600

    add tests for plain format

--- __tests__/src/genDiff.test.js
+++ __tests__/src/genDiff.test.js
@@ -2,6 +2,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import genDiff from '../../src/genDiff.js';
 import expected from '../../__fixtures__/expected.js';
+import expected2 from '../../__fixtures__/expected2.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -20,4 +21,12 @@ const yamlPath2 = getFixturePath('file2.yml');
 
 test('gendiff main flow, yaml', () => {
   expect(genDiff(yamlPath1, yamlPath2)).toBe(expected);
+});
+
+test('gendiff main flow, plain, json', () => {
+  expect(genDiff(jsonPath1, jsonPath2, 'plain')).toBe(expected2);
+});
+
+test('gendiff main flow, plain, yml', () => {
+  expect(genDiff(yamlPath1, yamlPath2, 'plain')).toBe(expected2);
 });
