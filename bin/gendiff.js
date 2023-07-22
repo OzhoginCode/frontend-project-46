44e1bf88cd5bb03494735db3e752baeb63996ac7
OzhoginCode <danozh.b@gmail.com>
2023-07-22 17:20:30 +0600

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,7 @@ import genDiff from '../src/genDiff.js';
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0')
+  .version('0.0.1')
   .argument('<filepath1>')
   .argument('<filepath2>')
   .option('-f, --format <type>', 'output format', 'stylish')
